db177825714b77c33763fd3e3cd53f9ee77e8306
Sebastien Lubrano <sebastienlubrano38@gmail.com>
2021-07-21 06:25:21 -0700

    testing fixes for honda accord nidec carstate checks

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -404,6 +404,7 @@ class CarState(CarStateBase):
 
     # all hondas except CRV, RDX and 2019 Odyssey@China use 0xe4 for steering
     checks = [(0xe4, 100)]
+    
     if CP.carFingerprint in [CAR.CRV, CAR.CRV_EU, CAR.ACURA_RDX, CAR.ODYSSEY_CHN]:
       checks = [(0x194, 100)]
 
@@ -425,8 +426,8 @@ class CarState(CarStateBase):
       ]
     
     if CP.carFingerprint in HONDA_NIDEC_SERIAL_STEERING:
-      checks += [("STEER_MOTOR_TORQUE",100), 
-                ("STEER_STATUS",100)]
+      checks += [("STEER_MOTOR_TORQUE", 100), 
+                ("STEER_STATUS", 100)]
       signals += [("MOTOR_TORQUE", "STEER_MOTOR_TORQUE", 0),
                   ("STEER_TORQUE_SENSOR", "STEER_STATUS", 0),
                   ("STEER_STATUS", "STEER_STATUS", 0)]
